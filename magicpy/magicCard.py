5b415e0f900ee368fbb92e9a16ba18dca35718c1
birbarino <ethan418sayers@gmail.com>
2022-03-05 23:50:22 -0800

    Changed to camelCase because... idk :(

--- magicpy/magicCard.py
+++ magicpy/magicCard.py
@@ -5,17 +5,15 @@ class MagicCard:
     The plan is to have the program pass in set+name, a dictionary/fast data
     structure is used to find the card in the given set, then it is passed 
     to a getter that returns all of the other data.
-
-    
     '''
     def __init__(self, name="None", set="None"):
         self.name = name
         self.set = set
 
         # TODO: Implement and return card getter/setter for following vars
-        self.manacost = ""
+        self.manaCost = ""
         self.type = ""
-        self.power_toughness = ()
+        self.powerToughness = ()
         self.rarity = ""
         self.oracleText = ""
         self.keywords = []  # would a set be better? "Haste haste" is possible
@@ -33,12 +31,12 @@ class MagicCard:
         # 5 = exile
         # 6 = command
 
-    def get_mana_value(self):
+    def getManaValue(self):
         # self.manaCost should always be formatted as "X{C}" where "X" is the 
         # total generic mana cost and "{C}" is each color in the mana cost.
-        return int(self.manacost[0]) + len(self.manacost[:1])
+        return int(self.manaCost[0]) + len(self.manaCost[:1])
 
     # TODO: Finish method, incl planeswalker attacking 
-    def attack(self, attackable_object):
+    def attack(self, attackableObject):
         print("TODO: Define MagicCard.attack() method.")
         pass
