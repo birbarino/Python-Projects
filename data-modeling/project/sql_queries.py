3747c6b3c86c54368838d80295a936334cf06a0b
Ethan <ethan418sayers@gmail.com>
2025-08-16 14:18:40 -0500

    a

--- data-modeling/project/sql_queries.py
+++ data-modeling/project/sql_queries.py
@@ -1,49 +1,106 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays (
+        songplay_id int,
+        start_time timestamp,
+        user_id int,
+        level text,
+        song_id text,
+        artist_id text,
+        session_id int,
+        location text,
+        user_agent text,
+        PRIMARY KEY (songplay_id)
+    );
 """)
 
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id int,
+        first_name text,
+        last_name text,
+        gender text,
+        level text,
+        PRIMARY KEY (user_id)
+    );
 """)
 
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs (
+        song_id text,
+        title text,
+        artist_id text,
+        year int,
+        duration numeric,
+        PRIMARY KEY (song_id)
+    );
 """)
 
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists (
+        artist_id text,
+        name text,
+        location text,
+        latitude numeric,
+        longitude numeric,
+        PRIMARY KEY (artist_id)
+    );
 """)
 
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time (
+        start_time timestamp,
+        hour int,
+        day int,
+        month int,
+        year int,
+        weekday int,
+        PRIMARY KEY (start_time)
+    );
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+    INSERT INTO songplay 
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
 """)
 
 user_table_insert = ("""
+    INSERT INTO users 
+    VALUES (%s, %s, %s, %s, %s);
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs (song_id, title, artist_id, duration) 
+    VALUES (%s, %s, %s, %s);
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artists (artist_id, name, location, latitude, longitude) 
+    VALUES (%s, %s, %s, %s, %s);
+                       
 """)
 
 
 time_table_insert = ("""
+    INSERT INTO time 
+    VALUES (%s, %s, %s, %s, %s, %s);
 """)
 
 # FIND SONGS
 
 song_select = ("""
+    
 """)
 
 # QUERY LISTS
