b1ee1f543dce40a83704f8af91652188f0edb983
Ethan <ethan418sayers@gmail.com>
2025-08-20 21:46:59 -0500

    agh

--- data-modeling/project/sql_queries.py
+++ data-modeling/project/sql_queries.py
@@ -13,15 +13,9 @@ songplay_table_create = ("""
         songplay_id int,
         start_time timestamp,
         user_id int,
-<<<<<<< HEAD
         level text,
         song_id text,
         artist_id text,
-=======
-        level int,
-        song_id int,
-        artist_id int,
->>>>>>> 79bbf51b52144a72f679aaea7fc64766f91c708a
         session_id int,
         location text,
         user_agent text,
@@ -35,26 +29,16 @@ user_table_create = ("""
         first_name text,
         last_name text,
         gender text,
-<<<<<<< HEAD
         level text,
-=======
-        level int,
->>>>>>> 79bbf51b52144a72f679aaea7fc64766f91c708a
         PRIMARY KEY (user_id)
     );
 """)
 
 song_table_create = ("""
     CREATE TABLE IF NOT EXISTS songs (
-<<<<<<< HEAD
         song_id text,
         title text,
         artist_id text,
-=======
-        song_id int,
-        title text,
-        artist_id int,
->>>>>>> 79bbf51b52144a72f679aaea7fc64766f91c708a
         year int,
         duration numeric,
         PRIMARY KEY (song_id)
@@ -63,11 +47,7 @@ song_table_create = ("""
 
 artist_table_create = ("""
     CREATE TABLE IF NOT EXISTS artists (
-<<<<<<< HEAD
         artist_id text,
-=======
-        artist_id int,
->>>>>>> 79bbf51b52144a72f679aaea7fc64766f91c708a
         name text,
         location text,
         latitude numeric,
@@ -91,7 +71,6 @@ time_table_create = ("""
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-<<<<<<< HEAD
     INSERT INTO songplay 
     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
 """)
@@ -110,31 +89,12 @@ artist_table_insert = ("""
     INSERT INTO artists (artist_id, name, location, latitude, longitude) 
     VALUES (%s, %s, %s, %s, %s);
                        
-=======
-    INSERT INTO songplay VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
-""")
-
-user_table_insert = ("""
-    INSERT INTO users VALUES (%s, %s, %s, %s, %s);
-""")
-
-song_table_insert = ("""
-    INSERT INTO songs (song_id, title, artist_id, duration) VALUES (%s, %s, %s, %s, %s);
-""")
-
-artist_table_insert = ("""
-    INSERT INTO artists VALUES (%s, %s, %s, %s, %s);
->>>>>>> 79bbf51b52144a72f679aaea7fc64766f91c708a
 """)
 
 
 time_table_insert = ("""
-<<<<<<< HEAD
     INSERT INTO time 
     VALUES (%s, %s, %s, %s, %s, %s);
-=======
-    INSERT INTO time VALUES (%s, %s, %s, %s, %s, %s);
->>>>>>> 79bbf51b52144a72f679aaea7fc64766f91c708a
 """)
 
 # FIND SONGS
