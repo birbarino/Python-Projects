6c5095cbe6f96cec928547c0718d2f883f5137a3
Ethan <ethan418sayers@gmail.com>
2025-11-29 21:10:32 -0600

    aaa

--- ml-devops/jupyter-server.sh
+++ ml-devops/jupyter-server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-conda init && conda activate udacity
 # Get eth0 IP address
 HOST_ETH0_IP=`ip addr | grep eth0 | grep inet | awk '{print $2}' | cut -d"/" -f1`
 # Run jupyter notebook for the IP address
-jupyter notebook --no-browser --ip $HOST_ETH0_IP
+jupyter lab --NotebookApp.token=$JUPYTER_TOKEN --no-browser --ip $HOST_ETH0_IP
